712bc89a250fc8e373c5de6bc596bf926ea5d1b5
padosum <makalu171@gmail.com>
2022-07-07 16:16:49 +0900

    ✨ 휴대번호 입력시 하이픈 추가 작업 #24

--- public/javascripts/certification.js
+++ public/javascripts/certification.js
@@ -2,19 +2,58 @@ const handleInputFocusChange = (e) => {
   e.target.closest("label").classList.add("focus");
 };
 
-window.addEventListener("click", (e) => {
-  const inputs = document.querySelectorAll("input");
-  if (e.target.tagName === "INPUT") {
-    return;
-  }
-  for (const input of inputs) {
-    input.closest("label").classList.remove("focus");
-  }
-});
+const handlePhoneInputHyphen = (e) => {
+  e.target.value = e.target.value
+    .replace(/[^0-9]/g, "")
+    .replace(
+      /(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,
+      "$1-$2-$3"
+    )
+    .replace("--", "-");
+};
 
-window.addEventListener("load", () => {
-  const inputs = document.querySelectorAll("input");
-  for (const input of inputs) {
-    input.addEventListener("focus", handleInputFocusChange);
-  }
-});
+function inputEl($page, name) {
+  this.$target = $page.querySelector(name);
+  this.init = () => {
+    initEventListeners();
+  };
+
+  const initEventListeners = () => {
+    this.$target.addEventListener("focus", handleInputFocusChange);
+  };
+
+  this.setEvent = (type, listener) => {
+    this.$target.addEventListener(type, listener);
+  };
+
+  this.init();
+}
+
+function certificationPage($target) {
+  this.init = () => {
+    const inputPhone = new inputEl($target, "#phone");
+    inputPhone.setEvent("keyup", handlePhoneInputHyphen);
+
+    const certificationNumber = new inputEl($target, "#cNumber");
+
+    initEventListeners();
+  };
+
+  const initEventListeners = () => {
+    const inputs = $target.querySelectorAll("input");
+
+    $target.addEventListener("click", (e) => {
+      if (e.target.tagName === "INPUT") {
+        return;
+      }
+      for (const input of inputs) {
+        input.closest("label").classList.remove("focus");
+      }
+    });
+  };
+}
+
+const page = new certificationPage(
+  document.querySelector(".certification-wrapper")
+);
+page.init();
