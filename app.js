48835ea17602de5a9a5660aef05eb72e415a1663
padosum <makalu171@gmail.com>
2022-07-07 22:18:25 +0900

    🔧 port 설정

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen("3000", () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("Start App");
 });
